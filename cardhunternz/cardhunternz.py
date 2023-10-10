6eb52b56aaaf37c5f94deecc9cf7c2f13b94662f
scib3ria <karel.doorman7@gmail.com>
2023-10-10 15:57:22 +1300

    minor formatting changes

--- cardhunternz/cardhunternz.py
+++ cardhunternz/cardhunternz.py
@@ -16,26 +16,27 @@ class CardHunter:
         if not self.games:
             self.games = [i for i in ['MTG Single', 'Flesh And Blood Single'] if input(f'Are you searching for {i}s? (y/n): ') == 'y']
         self.stores = [
-            HobbyMasterStore(url='https://hobbymaster.co.nz/cards/get-cards', name='HobbyMaster', games=self.games),
             BayDragonStore(url='https://www.baydragon.co.nz/searchsingle/category/01', name='BayDragon', games=self.games),
-            RookGamingStore(url='https://e734ef.myshopify.com', name='Rook Gaming', games=self.games),
-            FabArmoryStore(url="https://fabarmoury-com.myshopify.com", name='fabarmory.com', games=self.games),
             ShopifyStore(url='bea-dnd.myshopify.com', name='BeaDndGames', games=self.games),
             ShopifyStore(url='calico-keep.myshopify.com', name='Calico Keep', games=self.games),
             ShopifyStore(url='cardbard.myshopify.com', name='Card Bard', games=self.games),
             ShopifyStore(url='chloetest.myshopify.com', name='Card Merchant', games=self.games),
+            ShopifyStore(url='card-merchant-christchurch.myshopify.com', name='Card Merchant Christchurch', games=self.games),
+            ShopifyStore(url='card-merchant-hamilton.myshopify.com', name='Card Merchant Hamilton', games=self.games),
             ShopifyStore(url='card-merchant-nelson.myshopify.com', name='Card Merchant Nelson', games=self.games),
+            ShopifyStore(url='kidsandmore.myshopify.com', name='Card Merchant Takapuna', games=self.games),
+            ShopifyStore(url='card-merchant-tauranga.myshopify.com', name='Card Merchant Tauranga', games=self.games),
+            FabArmoryStore(url='https://fabarmoury-com.myshopify.com', name='fabarmory.com', games=self.games),
             ShopifyStore(url='goblingames.myshopify.com', name='Goblin Games', games=self.games),
+            HobbyMasterStore(url='https://hobbymaster.co.nz/cards/get-cards', name='HobbyMaster', games=self.games),
             ShopifyStore(url='iron-knight-gaming.myshopify.com', name='Iron Knight Gaming', games=self.games),
             ShopifyStore(url='magicatwillis.myshopify.com', name='Magic at Willis', games=self.games),
+            ShopifyStore(url='novagames-nz.myshopify.com', name='Nova Games', games=self.games),
+            RookGamingStore(url='https://e734ef.myshopify.com', name='Rook Gaming', games=self.games),
             ShopifyStore(url='shuffle-n-cut-hobbies-games.myshopify.com', name='Shuffle and Cut Games', games=self.games),
             ShopifyStore(url='spellboundgames.myshopify.com', name='Spellbound Games', games=self.games),
+            ShopifyStore(url='tcg-collector-nz.myshopify.com', name='TCG Collector', games=self.games),
             ShopifyStore(url='tcgculture.myshopify.com', name='TCG Culture', games=self.games),
-            ShopifyStore(url="card-merchant-hamilton.myshopify.com", name="Card Merchant Hamilton", games=self.games),
-            ShopifyStore(url="card-merchant-tauranga.myshopify.com", name="Card Merchant Tauranga", games=self.games),
-            ShopifyStore(url="card-merchant-christchurch.myshopify.com", name="Card Merchant Christchurch", games=self.games),
-            ShopifyStore(url="novagames-nz.myshopify.com", name="Nova Games", games=self.games),
-            ShopifyStore(url="tcg-collector-nz.myshopify.com", name="TCG Collector", games=self.games),
         ]
     
     def findCards(self, card_list):
