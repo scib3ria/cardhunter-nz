082f39d0d55517093107491bcd0acc9b7e7dc095
big-benkle <benkle21@gmail.com>
2023-11-23 22:03:49 +1300

    style: apply black, isort, flake8

--- cardhunternz/cardhunternz.py
+++ cardhunternz/cardhunternz.py
@@ -1,60 +1,150 @@
 import pandas as pd
 import requests
-from .cardstore import ShopifyStore, HobbyMasterStore, BayDragonStore, RookGamingStore, FabArmoryStore
+
+from .cardstore import (
+    BayDragonStore,
+    FabArmoryStore,
+    HobbyMasterStore,
+    RookGamingStore,
+    ShopifyStore,
+)
+
 
 class CardHunter:
-    # List of New Zealand stores selling trading card singles (specifically Magic: the Gathering and Flesh and Blood)
-    
+    # List of New Zealand stores selling trading card singles
+    # (specifically Magic: the Gathering and Flesh and Blood)
+
     def __init__(self, mtg_search=False, fab_search=False):
         self.data = pd.DataFrame()
         # Instantiates a requests session for connecting to store websites
         self.conn = requests.Session()
         self.games = []
-        if mtg_search: self.games.append('MTG Single')
-        if fab_search: self.games.append('Flesh And Blood Single')
+        if mtg_search:
+            self.games.append("MTG Single")
+        if fab_search:
+            self.games.append("Flesh And Blood Single")
         # Choose which games you want to search if no default set
         if not self.games:
-            self.games = [i for i in ['MTG Single', 'Flesh And Blood Single'] if input(f'Are you searching for {i}s? (y/n): ') == 'y']
+            self.games = [
+                i
+                for i in ["MTG Single", "Flesh And Blood Single"]
+                if input(f"Are you searching for {i}s? (y/n): ") == "y"
+            ]
         self.stores = [
-            BayDragonStore(url='https://www.baydragon.co.nz/searchsingle/category/01', name='BayDragon', games=self.games),
-            ShopifyStore(url='bea-dnd.myshopify.com', name='BeaDndGames', games=self.games),
-            ShopifyStore(url='calico-keep.myshopify.com', name='Calico Keep', games=self.games),
-            ShopifyStore(url='cardbard.myshopify.com', name='Card Bard', games=self.games),
-            ShopifyStore(url='chloetest.myshopify.com', name='Card Merchant', games=self.games),
-            ShopifyStore(url='card-merchant-christchurch.myshopify.com', name='Card Merchant Christchurch', games=self.games),
-            ShopifyStore(url='card-merchant-hamilton.myshopify.com', name='Card Merchant Hamilton', games=self.games),
-            ShopifyStore(url='card-merchant-nelson.myshopify.com', name='Card Merchant Nelson', games=self.games),
-            ShopifyStore(url='kidsandmore.myshopify.com', name='Card Merchant Takapuna', games=self.games),
-            ShopifyStore(url='card-merchant-tauranga.myshopify.com', name='Card Merchant Tauranga', games=self.games),
-            FabArmoryStore(url='https://fabarmoury-com.myshopify.com', name='fabarmory.com', games=self.games),
-            ShopifyStore(url='goblingames.myshopify.com', name='Goblin Games', games=self.games),
-            HobbyMasterStore(url='https://hobbymaster.co.nz/cards/get-cards', name='HobbyMaster', games=self.games),
-            ShopifyStore(url='iron-knight-gaming.myshopify.com', name='Iron Knight Gaming', games=self.games),
-            ShopifyStore(url='magicatwillis.myshopify.com', name='Magic at Willis', games=self.games),
-            ShopifyStore(url='novagames-nz.myshopify.com', name='Nova Games', games=self.games),
-            RookGamingStore(url='https://e734ef.myshopify.com', name='Rook Gaming', games=self.games),
-            ShopifyStore(url='shuffle-n-cut-hobbies-games.myshopify.com', name='Shuffle and Cut Games', games=self.games),
-            ShopifyStore(url='spellboundgames.myshopify.com', name='Spellbound Games', games=self.games),
-            ShopifyStore(url='tcg-collector-nz.myshopify.com', name='TCG Collector', games=self.games),
-            ShopifyStore(url='tcgculture.myshopify.com', name='TCG Culture', games=self.games),
+            BayDragonStore(
+                url="https://www.baydragon.co.nz/searchsingle/category/01",
+                name="BayDragon",
+                games=self.games,
+            ),
+            ShopifyStore(
+                url="bea-dnd.myshopify.com", name="BeaDndGames", games=self.games
+            ),
+            ShopifyStore(
+                url="calico-keep.myshopify.com", name="Calico Keep", games=self.games
+            ),
+            ShopifyStore(
+                url="cardbard.myshopify.com", name="Card Bard", games=self.games
+            ),
+            ShopifyStore(
+                url="chloetest.myshopify.com", name="Card Merchant", games=self.games
+            ),
+            ShopifyStore(
+                url="card-merchant-christchurch.myshopify.com",
+                name="Card Merchant Christchurch",
+                games=self.games,
+            ),
+            ShopifyStore(
+                url="card-merchant-hamilton.myshopify.com",
+                name="Card Merchant Hamilton",
+                games=self.games,
+            ),
+            ShopifyStore(
+                url="card-merchant-nelson.myshopify.com",
+                name="Card Merchant Nelson",
+                games=self.games,
+            ),
+            ShopifyStore(
+                url="kidsandmore.myshopify.com",
+                name="Card Merchant Takapuna",
+                games=self.games,
+            ),
+            ShopifyStore(
+                url="card-merchant-tauranga.myshopify.com",
+                name="Card Merchant Tauranga",
+                games=self.games,
+            ),
+            FabArmoryStore(
+                url="https://fabarmoury-com.myshopify.com",
+                name="fabarmory.com",
+                games=self.games,
+            ),
+            ShopifyStore(
+                url="goblingames.myshopify.com", name="Goblin Games", games=self.games
+            ),
+            HobbyMasterStore(
+                url="https://hobbymaster.co.nz/cards/get-cards",
+                name="HobbyMaster",
+                games=self.games,
+            ),
+            ShopifyStore(
+                url="iron-knight-gaming.myshopify.com",
+                name="Iron Knight Gaming",
+                games=self.games,
+            ),
+            ShopifyStore(
+                url="magicatwillis.myshopify.com",
+                name="Magic at Willis",
+                games=self.games,
+            ),
+            ShopifyStore(
+                url="novagames-nz.myshopify.com", name="Nova Games", games=self.games
+            ),
+            RookGamingStore(
+                url="https://e734ef.myshopify.com", name="Rook Gaming", games=self.games
+            ),
+            ShopifyStore(
+                url="shuffle-n-cut-hobbies-games.myshopify.com",
+                name="Shuffle and Cut Games",
+                games=self.games,
+            ),
+            ShopifyStore(
+                url="spellboundgames.myshopify.com",
+                name="Spellbound Games",
+                games=self.games,
+            ),
+            ShopifyStore(
+                url="tcg-collector-nz.myshopify.com",
+                name="TCG Collector",
+                games=self.games,
+            ),
+            ShopifyStore(
+                url="tcgculture.myshopify.com", name="TCG Culture", games=self.games
+            ),
         ]
-    
+
     def findCards(self, card_list):
         # Updates the data with search results from each store
         results = []
         for store in self.stores:
             store.findCards(card_list)
             results.append(store.get_dataframe())
-        self.data = pd.concat(results, ignore_index=True).sort_values(by=['card_name', 'price']).reset_index(drop=True)
-    
+        self.data = (
+            pd.concat(results, ignore_index=True)
+            .sort_values(by=["card_name", "price"])
+            .reset_index(drop=True)
+        )
+
     def cheapestPrices(self, pandas=True):
-        cheapest_cards = self.data.loc[self.data.groupby('card_name')['price'].idxmin(), :]
+        cheapest_cards = self.data.loc[
+            self.data.groupby("card_name")["price"].idxmin(), :
+        ]
         if pandas:
             return cheapest_cards
         else:
             return f"""{cheapest_cards.to_string()}
 
-The lowest total cost for these cards (excluding shipping costs) is ${cheapest_cards['price'].sum()}"""
+The lowest total cost for these cards (excluding shipping costs) is
+${cheapest_cards['price'].sum()}"""
 
     def allPrices(self):
         return self.data
--- cardhunternz/cardstore.py
+++ cardhunternz/cardstore.py
@@ -1,15 +1,17 @@
 from abc import ABC, abstractmethod
+
 import pandas as pd
 import requests
-from bs4 import BeautifulSoup
 from alive_progress import alive_bar
+from bs4 import BeautifulSoup
+
 
 class CardStore(ABC):
     def __init__(self, url, name, games, skip_art_cards=True):
         self.url = url
         self.name = name
         self.games = games
-        self.skip_art_cards=skip_art_cards
+        self.skip_art_cards = skip_art_cards
         self.conn = requests.Session()
         self.data = {}
 
@@ -17,9 +19,11 @@ class CardStore(ABC):
         # Updates the data with each card in card_list containing an empty dict
         self.data.update({k: {} for k in card_list})
 
-        with alive_bar(len(self.data.keys()), dual_line=True, title=self.name, title_length=21) as bar:
+        with alive_bar(
+            len(self.data.keys()), dual_line=True, title=self.name, title_length=21
+        ) as bar:
             for card_name in self.data.keys():
-                bar.text = f'Hunting for {card_name}'
+                bar.text = f"Hunting for {card_name}"
                 self.data[card_name] = self.storeSearch(card_name)
                 bar()
             return self.data
@@ -33,138 +37,193 @@ class CardStore(ABC):
         for search_name, stock in self.data.items():
             for variant in stock:
                 flat_json = {
-                    'card_name': search_name,
-                    'store': self.name,
-                    'version': variant['Name'],
-                    'price': variant['Price'],
-                    'quantity': variant['Quantity']
+                    "card_name": search_name,
+                    "store": self.name,
+                    "version": variant["Name"],
+                    "price": variant["Price"],
+                    "quantity": variant["Quantity"],
                 }
                 flat_list.append(flat_json)
         df = pd.DataFrame.from_records(flat_list)
         return df
+
 
 class ShopifyStore(CardStore):
     # Queries BinderPOS API for each store  and processes the resulting data
     def storeSearch(self, card_name):
         card_results = []
         data = {
-            'productTypes': self.games,
-            'instockOnly': True,
-            'storeUrl': self.url,
-            'strict': True,
-            'limit': 100,
-            'title': card_name,
-            'quantityGreaterThan': 0,
+            "productTypes": self.games,
+            "instockOnly": True,
+            "storeUrl": self.url,
+            "strict": True,
+            "limit": 100,
+            "title": card_name,
+            "quantityGreaterThan": 0,
         }
-        resp = self.conn.post(url = 'https://portal.binderpos.com/external/shopify/products/forStore', json=data).json()
+        resp = self.conn.post(
+            url="https://portal.binderpos.com/external/shopify/products/forStore",
+            json=data,
+        )
+        try:
+            resp = resp.json()
+        except Exception:
+            if "You are being rate limited" in str(resp.content):
+                print("Blocked by Cloudflare")
+            else:
+                print("unexpected exception")
+                print(resp.content)
+            return []
 
-        for product in resp['products']:
-                if product['overallQuantity']:
-                    for variant in product['variants']:
-                        if variant['quantity'] > 0:
-                            # skip art cards
-                            if ("art card" in product["title"].lower()) and self.skip_art_cards:
-                                continue
-                            card_results.append({
-                                'Name': '{} - {}'.format(product["title"], variant["title"]),
-                                'Price': variant['price'],
-                                'Quantity': variant['quantity']
-                            })
+        for product in resp["products"]:
+            if product["overallQuantity"]:
+                for variant in product["variants"]:
+                    if variant["quantity"] > 0:
+                        # skip art cards
+                        if (
+                            "art card" in product["title"].lower()
+                        ) and self.skip_art_cards:
+                            continue
+                        card_results.append(
+                            {
+                                "Name": "{} - {}".format(
+                                    product["title"], variant["title"]
+                                ),
+                                "Price": variant["price"],
+                                "Quantity": variant["quantity"],
+                            }
+                        )
         return card_results
+
 
 class HobbyMasterStore(CardStore):
     def storeSearch(self, card_name):
         card_results = []
         data = []
         for game in self.games:
-            game_number = '1' if game == 'MTG Single' else '37'
-            hb_data = self.conn.get(f'{self.url}?lang=&game={game_number}$foil=&_search=true&name={card_name}').json()
-            if 'rows' in hb_data:
-                data.extend(hb_data['rows'])
-        # The data from Hobbymaster is stored a little strangely. Each cell is a list containing 12 items related to a card entry
-        # The cell[0] is the card name, cell[9] is the card condition, cell[10] is the card price and cell[12] is the card stock
+            game_number = "1" if game == "MTG Single" else "37"
+            hb_data = self.conn.get(
+                f"{self.url}?lang=&game={game_number}$foil=&_search=true&name={card_name}"
+            ).json()
+            if "rows" in hb_data:
+                data.extend(hb_data["rows"])
+        # The data from Hobbymaster is stored a little strangely.
+        # Each cell is a list containing 12 items related to a card entry
+        # cell[0] is the card name
+        # cell[9] is the card condition
+        # cell[10] is the card price
+        # cell[12] is the card stock
         for result in data:
-            if result['cell'][12] != 0:
+            if result["cell"][12] != 0:
                 # replace '8+' string with int
-                quantity = result['cell'][12]
-                if quantity == '8+': quantity = 8
+                quantity = result["cell"][12]
+                if quantity == "8+":
+                    quantity = 8
                 # skip art cards
                 if ("art card" in result["cell"][0].lower()) and self.skip_art_cards:
-                            continue
-                card_results.append({
-                    'Name': f'{result["cell"][0]} [{result["cell"][1]}] {result["cell"][9]}',
-                    'Price': float(result['cell'][10].replace('$', '')),
-                    'Quantity': quantity
-                })
+                    continue
+                card_results.append(
+                    {
+                        "Name": f'{result["cell"][0]} [{result["cell"][1]}] {result["cell"][9]}',
+                        "Price": float(result["cell"][10].replace("$", "")),
+                        "Quantity": quantity,
+                    }
+                )
         return card_results
-    
+
+
 class BayDragonStore(CardStore):
     def storeSearch(self, card_name):
         card_results = []
         # baydragon does not currently sell Flesh and Blood singles
-        game_number = '01' if 'MTG Single' in self.games else '25'
-        params = {'searchType': 'single', 'searchString': card_name}
-        html_content = self.conn.get(f'{self.url}/brand/{game_number}', params=params).text
+        game_number = "01" if "MTG Single" in self.games else "25"
+        params = {"searchType": "single", "searchString": card_name}
+        html_content = self.conn.get(
+            f"{self.url}/brand/{game_number}", params=params
+        ).text
         data = BeautifulSoup(html_content, "lxml")
         # HTML parsing of the results table
-        table = data.find('table', attrs={'id': ['tcgSingles', 'magicSingles']})
-        rows = table.find_all('tr')
+        table = data.find("table", attrs={"id": ["tcgSingles", "magicSingles"]})
+        rows = table.find_all("tr")
         for row in rows:
-            cols = row.find_all('td')
+            cols = row.find_all("td")
             d = [ele.text.strip() for ele in cols if ele]
             # Filters out the column headings and any cards that are out of stock
-            if d[7] not in ['Onhand', '0']:
-
+            if d[7] not in ["Onhand", "0"]:
                 # skip art cards
                 if ("art card" in d[1].lower()) and self.skip_art_cards:
                     continue
 
-                card_results.append({
-                    'Name': "{} [{}] - {}".format(d[1], d[2], d[5]),
-                    'Price': float(d[6].replace('NZ$', '')),
-                    'Quantity': d[7]
-                })
+                card_results.append(
+                    {
+                        "Name": "{} [{}] - {}".format(d[1], d[2], d[5]),
+                        "Price": float(d[6].replace("NZ$", "")),
+                        "Quantity": d[7],
+                    }
+                )
         return card_results
+
 
 class RookGamingStore(CardStore):
     def storeSearch(self, card_name):
         # Rook Gaming does not currently sell MTG singles
         card_results = []
-        if 'Flesh And Blood Single' not in self.games:
-            return card_results        
-        html_content = self.conn.get(f'{self.url}/search?q={card_name}').text
+        if "Flesh And Blood Single" not in self.games:
+            return card_results
+        html_content = self.conn.get(f"{self.url}/search?q={card_name}").text
         data = BeautifulSoup(html_content, "lxml")
         # HTML parsing of the results
-        search_results = data.find('div', attrs={'class': 'template-search__results'})
-        product_cards = search_results.find_all('div', attrs={'class': 'product-card product-grid'})
+        search_results = data.find("div", attrs={"class": "template-search__results"})
+        product_cards = search_results.find_all(
+            "div", attrs={"class": "product-card product-grid"}
+        )
         for product_card in product_cards:
             # Skip cards that are out of stock
-            if product_card.find('span', attrs = {'class': 'label-sold-out'}):
+            if product_card.find("span", attrs={"class": "label-sold-out"}):
                 continue
-            card_results.append({
-                'Name': product_card.find('h6', attrs = {'class': 'product-card__name'}).text.strip('\n'),
-                'Price': float(product_card.find('div', attrs = {'class': 'product-price'}).text.strip('$')),
-                'Quantity': 1 # Rook Gaming does not have card quantities on its results page
-            })
+            card_results.append(
+                {
+                    "Name": product_card.find(
+                        "h6", attrs={"class": "product-card__name"}
+                    ).text.strip("\n"),
+                    "Price": float(
+                        product_card.find(
+                            "div", attrs={"class": "product-price"}
+                        ).text.strip("$")
+                    ),
+                    "Quantity": 1,  # Rook Gaming does not have card quantities on its results page
+                }
+            )
         return card_results
-    
+
+
 class FabArmoryStore(CardStore):
     def storeSearch(self, card_name):
         # Fabarmory does not currently sell MTG singles
         card_results = []
-        if 'Flesh And Blood Single' not in self.games:
+        if "Flesh And Blood Single" not in self.games:
             return card_results
-        html_content = self.conn.get(f'{self.url}/search?q={card_name}').text
+        html_content = self.conn.get(f"{self.url}/search?q={card_name}").text
         data = BeautifulSoup(html_content, "lxml")
-        items = data.find('ul', attrs= {'class': 'page-width list-view-items'})
-        card_products = items.find_all('li')
+        items = data.find("ul", attrs={"class": "page-width list-view-items"})
+        card_products = items.find_all("li")
         for card_product in card_products:
             # Skip cards that are not in stock
-            if card_product.find('dl', attrs={'class': 'price--sold-out'}):
+            if card_product.find("dl", attrs={"class": "price--sold-out"}):
                 continue
-            card_results.append({
-                'Name': card_product.find('span', attrs={'class': 'product-card__title'}).text,
-                'Price': float(card_product.find('span', attrs={'class': 'price-item--regular'}).text.strip('$').strip()),
-                'Quantity': 1, # FAB Armory does not have card quantities on its results page
-            })
+            card_results.append(
+                {
+                    "Name": card_product.find(
+                        "span", attrs={"class": "product-card__title"}
+                    ).text,
+                    "Price": float(
+                        card_product.find(
+                            "span", attrs={"class": "price-item--regular"}
+                        )
+                        .text.strip("$")
+                        .strip()
+                    ),
+                    "Quantity": 1,  # FAB Armory does not have card quantities on its results page
+                }
+            )
         return card_results
